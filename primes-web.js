d1f377cdc3a85cb3c01f4fd9e91a389e2e48d66e
AlparDuman <>
2025-05-18 04:51:50 +0200

    initial with license

---
+++ primes-web.js
@@ -0,0 +1,19 @@
+/*
+    Copyright (C) 2025 Alpar Duman
+    This file is part of primes-web.
+
+    primes-web is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 3 as
+    published by the Free Software Foundation.
+
+    primes-web is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with primes-web. If not, see
+    <https://github.com/AlparDuman/primes-web/blob/main/LICENSE>
+    else <https://www.gnu.org/licenses/>.
+*/
+
